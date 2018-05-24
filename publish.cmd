a95e706c874f63a634570f017eaae21b717c1eda
Adam Stephensen <adsteph@microsoft.com>
2018-05-24 16:22:29 +1000

    initial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot-web-basic-v4-node-Web-Deploy.pubxml -p:Password=7RjkB6KSXTZphsuFgj7da4Ms19L0Lk8g7yKZHrW21yG5rdv98ZdNWjfRchq7
+
